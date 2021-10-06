bfa368de915546d06d69fd180c1a13a435e6965b
etienne912 <etienne9912@gmail.com>
2021-10-06 11:07:17 +0200

    dataFilter!

--- scripts/dataFilter.sh
+++ scripts/dataFilter.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-find . -name README  -type f -delete
+find . -name README -type f -delete
 find . -type f -size -5k -delete
-find . -name *.unk -type f -delete
+find . -name "*.unk" -type f -delete
 find . -type f -not -name "*.jpg" -not -name "*.png" -not -name "*.xml" -not -name "*.pdf" -not -name "*.doc" -not -name "*.csv" -not -name "*.html" -not -name "*.ppt" -not -name "*.xls" -not -name "*.gif" -not -name "*.txt" -not -name "*.ps" -not -name "*.gz" -not -name "*.gif" -delete
 find . -type f -size +20M -delete
 
