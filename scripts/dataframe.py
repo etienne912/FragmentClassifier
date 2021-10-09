2d8671fad8e7a61b1d3ae9863e36d9ebb587ce20
Maxence <maxence.lavergne@etu.univ-nantes.fr>
2021-10-09 09:34:54 +0200

    Black and white to color

--- scripts/dataframe.py
+++ scripts/dataframe.py
@@ -1,11 +1,10 @@
 import os
 import sys
-from pyspark.sql import SparkSession
 import cv2
 import numpy as np
 import pandas as pd
 import os
-
+import matplotlib.image as mpimg
 
 
 def file_to_fragment(path,return_path):
@@ -20,7 +19,7 @@ def file_to_fragment(path,return_path):
     df = pd.DataFrame({"class": file_class, "path": file_path})
     path_sample = return_path
 
-    for cls in ["csv", "pdf", "txt", "html", "doc", "ppt", "xls"]:
+    for cls in ["gz"]:
         path_folder = os.path.join(path_sample + "/" + cls)
         if not os.path.isdir(path_folder):
             os.mkdir(path_folder)
@@ -33,20 +32,28 @@ def file_to_fragment(path,return_path):
             if nb_images != 0:
                 file.read(512)
                 data = file.read(4096)
+
                 for i in range(nb_images):
                     flatNumpyArray = np.array(bytearray(data))
                     grayImage = flatNumpyArray.reshape(64, 64)
-                    cv2.imwrite(os.path.join(path_folder, str(count) + '.jpg'), grayImage)
+                    backtorgb = cv2.cvtColor(grayImage,cv2.COLOR_GRAY2RGB)
+                    cv2.imwrite(os.path.join(path_folder, str(count) + '.jpg'), backtorgb)
                     data = file.read(4096)
 
                     count += 1
 
+def read_fragment_to_dataframe(path:str):
+    cls = []
+    image = []
+    for subdirectory in os.walk(path):
+        for image in subdirectory[2]:
+            cls.append(subdirectory)
+            img = mpimg.imread(os.path.join(subdirectory[0], image))
+            print(img)
+
 if __name__ == '__main__':
-    spark = SparkSession.builder.appName("DL with Spark Deep Cognition").getOrCreate()
-    sc = spark.sparkContext
-    image_df = spark.read.format("image").load("../data/fragment/txt")
-
-
-
+    path_directory ="../data/fragment"
+    path = "../data/govdocs_selected"
+    file_to_fragment(path=path,return_path=path_directory)
 
 
